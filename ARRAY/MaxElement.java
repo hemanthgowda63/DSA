5018cf7190b791a310240969a49468ed7dd86d7b
hemanthgowda63 <132736954+hemanthgowda63@users.noreply.github.com>
2026-02-23 20:49:19 +0530

    Add files via upload

---
+++ ARRAY/MaxElement.java
@@ -0,0 +1,19 @@
+import java.util.Arrays;
+
+public class MaxElement {
+    public static void main(String[] args) {
+        int[] arr={1,2,3,4,5};
+        System.out.println(max(arr));
+
+    }
+    static int max(int[] arr){
+        int max=arr[0];
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]>max){
+                max=arr[i];
+            }
+        }
+        return max;
+    }
+
+}
