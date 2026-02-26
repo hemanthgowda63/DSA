b31489ac58dbfc745176a7b8b826aff216311334
hemanthgowda63 <132736954+hemanthgowda63@users.noreply.github.com>
2026-02-26 21:59:38 +0530

    Add files via upload

---
+++ MATHS for DSA/PowerOfTwo.java
@@ -0,0 +1,11 @@
+public class PowerOfTwo {
+        public static boolean isPowerOfTwo(int n) {
+            return n > 0 && (n & (n-1))== 0;
+
+        }
+
+        public static void main(String[] args) {
+            System.out.println(isPowerOfTwo(128));
+        }
+
+}
