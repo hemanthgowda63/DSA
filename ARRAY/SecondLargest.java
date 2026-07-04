717b27717b9fefbec63cb4cc5525e53b3fe575f0
hemanthgowda63 <132736954+hemanthgowda63@users.noreply.github.com>
2026-07-04 11:16:58 +0530

    Add files via upload

---
+++ ARRAY/SecondLargest.java
@@ -0,0 +1,19 @@
+package Array;
+
+public class SecondLargest {
+    public static void main(String[] args) {
+        int[] arr={80,80,20};
+        int largest=arr[0];
+        int secondLargest=Integer.MIN_VALUE;
+        for (int i = 1; i < arr.length ; i++) {
+            if(arr[i]>largest){
+                secondLargest=largest;
+                largest=arr[i];
+            }else if(arr[i]>secondLargest && arr[i]<largest){
+                secondLargest=arr[i];
+            }
+
+        }
+        System.out.println(secondLargest);
+    }
+}
